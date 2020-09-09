e564ead2e8b399345b01c90d8fd16bfd352ae170
fechad <fedwin.chatelier@polymtl.ca>
2020-09-09 19:39:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@ import math
 def square_root(number: int) -> float:
     # TODO completer la fonction
      number=25
-    return square_rooted= math.sqrt(number)
+    return square_rooted
 
 
 def square(number: int) -> int:
